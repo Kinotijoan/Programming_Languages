f0179e3664980924a79a74b5acadcac32674936a
Kinotijoan <kinotijoan@gmail.com>
2023-11-19 16:46:02 +0300

    OOP-Calculator

--- calculatorOOP.py
+++ calculatorOOP.py
@@ -3,6 +3,7 @@
 
 class Calculator:
     def __init__(self, num1, num2, operation):
+        self.validOps = ["+", "-", "*", "/"]
         self.num1 = num1
         self.num2 = num2
         self.operation = operation
@@ -15,17 +16,27 @@ class Calculator:
 
     def divide(self):
         try:
-          return self.num1 / self.num2
+           return self.num1 / self.num2
         except ZeroDivisionError:
-            print("Cannot divide by zero")
+            return("Cannot divide by zero")
 
     def multiply(self):
         return self.num1 * self.num2
 
     def getInput(self):
-        self.num1 = float(input("Enter the first number: "))
-        self.num2 = float(input("Enter the second number: "))
+        while True:
+            try:
+                self.num1 = float(input("Enter the first number: "))
+                self.num2 = float(input("Enter the second number: "))
+                break  
+            except ValueError:
+                print("Enter numbers only")
+
         self.operation = str(input("What operation do you want to perform on the numbers? "))
+        while self.operation not in self.validOps:
+                print("Invalid operation")
+                self.operation = input("What operation do you want to perform on the numbers? ")
+
 
     def eval(self):
         if self.operation == "+":
@@ -36,9 +47,6 @@ class Calculator:
             return self.multiply()
         elif self.operation == "/":
             return self.divide()
-        else:
-            print("Invalid operation")
-            self.operation = input("What operation do you want to perform on the numbers? ")
 
 
 def main():
